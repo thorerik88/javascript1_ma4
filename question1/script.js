b7a449411f632503474e23f4edea6c76a6ad4050
Thor-Erik <thorerik88@hotmail.com>
2019-12-11 21:34:58 +0100

    answered question2 in question1 script. fixed that

--- question1/script.js
+++ question1/script.js
@@ -1,36 +1,3 @@
 // display alert message, when button is clicked
 const button = document.querySelector("button");
 button.addEventListener("click", () => alert("The button works!"));
-
-// CREATE A LIST OF TODO CARDS ON A WEBPAGE
-
-// global variables
-function loopResults(json) {
-  const resultsElement = document.querySelector(".results");
-  console.log(json.length)
-  for (let key in json) {
-    // set output text for the div element inner html
-    text = "<h1>" + "UserId: " + json[key].userId + "</h1>";
-    text += "<h2>" + "id: " + json[key].id + "</h2>";
-    text += "<p>" + "title: " + json[key].title + "</p>";
-    text += "<p>" + "completed: " + json[key].completed + "</p>";
-
-    // create a div element, add text and append to main div element
-    let todo = document.createElement('div');
-    todo.innerHTML = text;
-    resultsElement.appendChild(todo);
-  }
-}
-
-// retreive the API
-async function callApi() {
-  try {
-    const response = await fetch("https://jsonplaceholder.typicode.com/todos");
-    const json = await response.json();
-    loopResults(json);
-  }
-  catch (error) {
-    console.log(error);
-  }
-}
-callApi();
